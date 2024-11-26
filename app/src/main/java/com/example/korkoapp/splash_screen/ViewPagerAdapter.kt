1c2f9a4518fc600b15065f59e6050378bded7ad8
JackyDom <sopheaoudom8@gmail.com>
2024-11-26 11:24:22 +0700

    commit today 26/11/2024

--- app/src/main/java/com/example/korkoapp/splash_screen/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/korkoapp/splash_screen/ViewPagerAdapter.kt
@@ -1,14 +1,65 @@
 package com.example.korkoapp.splash_screen
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.viewpager.widget.PagerAdapter
+import com.example.korkoapp.R
 
-class ViewPagerAdapter : PagerAdapter() {
+class ViewPagerAdapter(private val context: Context) : PagerAdapter() {
+    private val images = intArrayOf(
+        R.drawable.image3,
+        R.drawable.image4,
+        R.drawable.image5,
+        R.drawable.image6
+    )
+
+    private val headings = intArrayOf(
+        R.string.heading_one,
+        R.string.heading_two,
+        R.string.heading_three,
+        R.string.heading_fourth
+    )
+
+    private val descriptions = intArrayOf(
+        R.string.desc_one,
+        R.string.desc_two,
+        R.string.desc_three,
+        R.string.desc_fourth
+    )
+
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return headings.size
     }
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        TODO("Not yet implemented")
+        return view == object as LinearLayout
+    }
+
+    @NonNull
+    override fun instantiateItem(@NonNull container: ViewGroup, position: Int): Any {
+        val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(R.layout.slider_layout, container, false)
+
+        val slideTitleImage = view.findViewById<ImageView>(R.id.titleImage)
+        val slideHeading = view.findViewById<TextView>(R.id.texttitle)
+        val slideDescription = view.findViewById<TextView>(R.id.textdeccription)
+
+        slideTitleImage.setImageResource(images[position])
+        slideHeading.setText(headings[position])
+        slideDescription.setText(descriptions[position])
+
+        container.addView(view)
+
+        return view
+    }
+
+    override fun destroyItem(@NonNull container: ViewGroup, position: Int, @NonNull obj: Any) {
+        container.removeView(obj as LinearLayout)
     }
 }
