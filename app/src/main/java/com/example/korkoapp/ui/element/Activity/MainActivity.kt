5e1dac3f7fc0585559c1ccbfb918e83f26b78e08
JackyDom <sopheaoudom8@gmail.com>
2024-12-07 22:54:06 +0700

    Complete Temple, Cafe 07/12/2024 part2

--- app/src/main/java/com/example/korkoapp/ui/element/Activity/MainActivity.kt
+++ app/src/main/java/com/example/korkoapp/ui/element/Activity/MainActivity.kt
@@ -1,12 +1,11 @@
-package com.example.korkoapp
+package com.example.korkoapp.ui.element.Activity
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.korkoapp.R
 import com.example.korkoapp.ui.splash_screen.SplashScreenActivity
 
 class MainActivity : AppCompatActivity() {
